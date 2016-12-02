9712296c108aa9799b7b1f47c80352a54b969178
Atishay Jain <contact@atishay.me>
2016-12-02 08:57:10 -0800

    Getting the home page ported. Still WIP

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,22 @@
 ---
 # Concatenate all minified JavaScript files
 ---
-{% include_relative _partials/js/foo.js %}
+{% include_relative _partials/js/jquery-min.js %}
+{% if jekyll.environment == 'production' %}
+{% include_relative _partials/js/tether.min.js %}
+{% include_relative _partials/js/bootstrap.min.js %}
+{% include_relative _partials/js/jquery.mixitup.min.js %}
+{% include_relative _partials/js/wow.min.js %}
+{% include_relative _partials/js/owl.carousel.min.js %}
+{% include_relative _partials/js/main.min.js %}
+{% else %}
+{% include_relative _partials/js/tether.js %}
+{% include_relative _partials/js/bootstrap.js %}
+// Mixitup for sort and filter projects
+{% include_relative _partials/js/jquery.mixitup.js %}
+// Wow for Scroll effects
+{% include_relative _partials/js/wow.js %}
+// OWL Carousel allows multiple items in a single carousel
+{% include_relative _partials/js/owl.carousel.js %}
+{% include_relative _partials/js/main.js %}
+{% endif %}
