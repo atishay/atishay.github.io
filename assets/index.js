1eaa58247c2021e656fc31f6b06d87ec4d96d68b
Atishay Jain <contact@atishay.me>
2016-12-05 09:22:29 -0800

    Added pie chart

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,7 @@
 {% include_relative _partials/js/tether.min.js %}
 {% include_relative _partials/js/bootstrap.min.js %}
 {% include_relative _partials/js/jquery.mixitup.min.js %}
+{% include_relative _partials/js/jquery.drawPieChart.min.js %}
 {% include_relative _partials/js/wow.min.js %}
 {% include_relative _partials/js/owl.carousel.min.js %}
 {% include_relative _partials/js/main.min.js %}
@@ -14,6 +15,8 @@
 {% include_relative _partials/js/bootstrap.js %}
 // Mixitup for sort and filter projects
 {% include_relative _partials/js/jquery.mixitup.js %}
+//Draw pie chart on the home page
+{% include_relative _partials/js/jquery.drawPieChart.js %}
 // Wow for Scroll effects
 {% include_relative _partials/js/wow.js %}
 // OWL Carousel allows multiple items in a single carousel
