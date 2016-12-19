f7730a8abce78bc048855ac3785035aff51a9a00
Atishay Jain <contact@atishay.me>
2016-12-19 06:23:33 -0800

    Fix google map and own carousal.

--- assets/_partials/js/main.js
+++ assets/_partials/js/main.js
@@ -1,207 +1,205 @@
-
-//Dropdown Menus
-$('.dropdown').hover(
-  function () {
-    $(this).addClass('open');
-  },
-  function () {
-    $(this).removeClass('open');
-  }
-  );
-
-
-
-// //Search
-
-// var openSearch = $('.open-search'),
-//   SearchForm = $('.full-search'),
-//   closeSearch = $('.close-search');
-
-//   openSearch.on('click', function(event){
-//     event.preventDefault();
-//     if (!SearchForm.hasClass('active')) {
-//       SearchForm.fadeIn(300, function(){
-//         SearchForm.addClass('active');
-//       });
-//     }
-//   });
-
-//   closeSearch.on('click', function(event){
-//     event.preventDefault();
-
-//     SearchForm.fadeOut(300, function(){
-//       SearchForm.removeClass('active');
-//       $(this).find('input').val('');
-//     });
-//   });
-
-
-//WOW Scroll Spy
-var wow = new WOW({
-    //disabled for mobile
-    mobile: false
-});
-wow.init();
-
-
-//Owl Carousel
-$('#clients-scroller').owlCarousel({
-    items:4,
-    itemsTablet:3,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-});
-
-//Color Client
-$('#color-client-scroller').owlCarousel({
-    items:4,
-    itemsTablet:3,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-});
-
-//Owl Carousel
-$('#testimonial-item').owlCarousel({
-    autoPlay: 5000,
-    items:3,
-    itemsTablet:3,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-});
-
-//Dark Testimonial Carousel
-$('#testimonial-dark').owlCarousel({
-    autoPlay: 5000,
-    items:3,
-    itemsTablet:3,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-});
-
-// Single Testimonial
-$('#single-testimonial-item').owlCarousel({
-  singleItem: true,
-  autoPlay: 5000,
-    items: 1,
-    itemsTablet: 1,
-    margin:90,
-    stagePadding:90,
-    smartSpeed:450,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,3],
-    itemsTablet: [767,2],
-    itemsTabletSmall: [480,2],
-    itemsMobile : [479,1],
-    stopOnHover: true,
-});
-
-// Image Carousel
-$('#image-carousel').owlCarousel({
-  autoPlay: 3000, //Set AutoPlay to 3 seconds
-  items : 4,
-  itemsDesktop : [1170,3],
-  itemsDesktopSmall : [1170,3]
-
-});
-
-// Slider Carousel
-$('#carousel-image-slider').owlCarousel({
-  navigation : false, // Show next and prev buttons
-  slideSpeed : 300,
-  paginationSpeed : 400,
-  singleItem:true,
-  pagination: false,
-  autoPlay: 3000,
-});
-
-
- //About owl carousel Slider
-  $(document).ready(function(){
-    /*=== About us ====*/
-    $('#carousel-about-us').owlCarousel({
-        navigation: true, // Show next and prev buttons
-        navigationText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-        slideSpeed: 800,
-        paginationSpeed: 400,
-        autoPlay: true,
-        singleItem: true,
-        pagination : false,
-        items : 1,
-        itemsCustom : false,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
-        itemsTabletSmall: false,
-        itemsMobile : [479,1],
-    });
-
-});
-
-//MixitUp
-$(function(){
-  $('#portfolio-list').mixItUp();
-});
-
-// Testimonial
-$('testimonial-carousel').carousel();
-$('a[data-slide="prev"]').click(function () {
-    $('#testimonial-carousel').carousel('prev');
-});
-
-$('a[data-slide="next"]').click(function () {
-    $('#testimonial-carousel').carousel('next');
-});
-
-//CounterUp - needs counterup.js
-// jQuery(document).ready(function( $ ) {
-//     $('.counter').counterUp({
-//         delay: 1,
-//         time: 800
-//     });
-// });
-
-// Progress Bar - Include Waypoints if this is needed.
-// $('.skill-shortcode').appear(function() {
-//   $('.progress').each(function() {
-//     $('.progress-bar').css('width', function() {
-//       return ($(this).attr('data-percentage') + '%')
-//     });
-//   });
-// }, {
-//   accY: -100
-// });
-
-
 $(function() {
+  //Dropdown Menus
+  $('.dropdown').hover(
+    function () {
+      $(this).addClass('open');
+    },
+    function () {
+      $(this).removeClass('open');
+    }
+    );
+
+
+
+  // //Search
+
+  // var openSearch = $('.open-search'),
+  //   SearchForm = $('.full-search'),
+  //   closeSearch = $('.close-search');
+
+  //   openSearch.on('click', function(event){
+  //     event.preventDefault();
+  //     if (!SearchForm.hasClass('active')) {
+  //       SearchForm.fadeIn(300, function(){
+  //         SearchForm.addClass('active');
+  //       });
+  //     }
+  //   });
+
+  //   closeSearch.on('click', function(event){
+  //     event.preventDefault();
+
+  //     SearchForm.fadeOut(300, function(){
+  //       SearchForm.removeClass('active');
+  //       $(this).find('input').val('');
+  //     });
+  //   });
+
+
+  //WOW Scroll Spy
+  var wow = new WOW({
+      //disabled for mobile
+      mobile: false
+  });
+  wow.init();
+
+
+  //Owl Carousel
+  $('#clients-scroller').owlCarousel({
+      items:4,
+      itemsTablet:3,
+      margin:90,
+      stagePadding:90,
+      smartSpeed:450,
+      itemsDesktop : [1199,4],
+      itemsDesktopSmall : [980,3],
+      itemsTablet: [768,3],
+      itemsTablet: [767,2],
+      itemsTabletSmall: [480,2],
+      itemsMobile : [479,1],
+  });
+
+  //Color Client
+  $('#color-client-scroller').owlCarousel({
+      items:4,
+      itemsTablet:3,
+      margin:90,
+      stagePadding:90,
+      smartSpeed:450,
+      itemsDesktop : [1199,4],
+      itemsDesktopSmall : [980,3],
+      itemsTablet: [768,3],
+      itemsTablet: [767,2],
+      itemsTabletSmall: [480,2],
+      itemsMobile : [479,1],
+  });
+
+  //Owl Carousel
+  $('#testimonial-item').owlCarousel({
+      autoPlay: 5000,
+      items:3,
+      itemsTablet:3,
+      margin:90,
+      stagePadding:90,
+      smartSpeed:450,
+      itemsDesktop : [1199,4],
+      itemsDesktopSmall : [980,3],
+      itemsTablet: [768,3],
+      itemsTablet: [767,2],
+      itemsTabletSmall: [480,2],
+      itemsMobile : [479,1],
+  });
+
+  //Dark Testimonial Carousel
+  $('#testimonial-dark').owlCarousel({
+      autoPlay: 5000,
+      items:3,
+      itemsTablet:3,
+      margin:90,
+      stagePadding:90,
+      smartSpeed:450,
+      itemsDesktop : [1199,4],
+      itemsDesktopSmall : [980,3],
+      itemsTablet: [768,3],
+      itemsTablet: [767,2],
+      itemsTabletSmall: [480,2],
+      itemsMobile : [479,1],
+  });
+
+  // Single Testimonial
+  $('#single-testimonial-item').owlCarousel({
+    singleItem: true,
+    autoPlay: 5000,
+      items: 1,
+      itemsTablet: 1,
+      margin:90,
+      stagePadding:90,
+      smartSpeed:450,
+      itemsDesktop : [1199,4],
+      itemsDesktopSmall : [980,3],
+      itemsTablet: [768,3],
+      itemsTablet: [767,2],
+      itemsTabletSmall: [480,2],
+      itemsMobile : [479,1],
+      stopOnHover: true,
+  });
+
+  // Image Carousel
+  $('#image-carousel').owlCarousel({
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+    items : 4,
+    itemsDesktop : [1170,3],
+    itemsDesktopSmall : [1170,3]
+
+  });
+
+  // Slider Carousel
+  $('#carousel-image-slider').owlCarousel({
+    navigation : false, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true,
+    pagination: false,
+    autoPlay: 3000,
+  });
+
+
+   //About owl carousel Slider
+    $(document).ready(function(){
+      /*=== About us ====*/
+      $('#carousel-about-us').owlCarousel({
+          navigation: true, // Show next and prev buttons
+          navigationText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+          slideSpeed: 800,
+          paginationSpeed: 400,
+          autoPlay: true,
+          singleItem: true,
+          pagination : false,
+          items : 1,
+          itemsCustom : false,
+          itemsDesktop : [1199,4],
+          itemsDesktopSmall : [980,3],
+          itemsTablet: [768,2],
+          itemsTabletSmall: false,
+          itemsMobile : [479,1],
+      });
+
+  });
+
+  //MixitUp
+  $('#portfolio-list').mixItUp();
+
+  // Testimonial
+  $('testimonial-carousel').carousel();
+  $('a[data-slide="prev"]').click(function () {
+      $('#testimonial-carousel').carousel('prev');
+  });
+
+  $('a[data-slide="next"]').click(function () {
+      $('#testimonial-carousel').carousel('next');
+  });
+
+  //CounterUp - needs counterup.js
+  // jQuery(document).ready(function( $ ) {
+  //     $('.counter').counterUp({
+  //         delay: 1,
+  //         time: 800
+  //     });
+  // });
+
+  // Progress Bar - Include Waypoints if this is needed.
+  // $('.skill-shortcode').appear(function() {
+  //   $('.progress').each(function() {
+  //     $('.progress-bar').css('width', function() {
+  //       return ($(this).attr('data-percentage') + '%')
+  //     });
+  //   });
+  // }, {
+  //   accY: -100
+  // });
+
+
+
   // Back Top Link
   var offset = 200;
   var duration = 500;
