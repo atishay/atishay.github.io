a4f05aecd881e38ce51b63656d90d5469da71511
Atishay Jain <contact@atishay.me>
2018-05-22 09:01:04 -0700

    Almost complete guitar section.

--- pre-push.sh
+++ pre-push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Minify main.js
 ./node_modules/.bin/uglifyjs assets/_partials/js/main.js -o assets/_partials/js/main.min.js
+./node_modules/.bin/uglifyjs assets/js/jtab.js -o assets/js/jtab.min.js
+./node_modules/.bin/uglifyjs assets/js/raphael.js -o assets/js/raphael.min.js
 # Add main.js to the cache
 git add assets/_partials/js/main.min.js
 
