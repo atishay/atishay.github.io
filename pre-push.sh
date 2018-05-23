3a8a0f56eab7beaa1633d6535c1b2001ce063db0
Atishay Jain <contact@atishay.me>
2018-05-22 21:12:10 -0700

    Update main.js

--- pre-push.sh
+++ pre-push.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Minify main.js
 ./node_modules/.bin/uglifyjs assets/_partials/js/main.js -o assets/_partials/js/main.min.js
-./node_modules/.bin/uglifyjs assets/js/jtab.js -o assets/js/jtab.min.js
-./node_modules/.bin/uglifyjs assets/js/raphael.js -o assets/js/raphael.min.js
 # Add main.js to the cache
 git add assets/_partials/js/main.min.js
 
