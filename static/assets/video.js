770fec3d39b7f2d5e72701a5c0c9ef7797f7b206
Atishay Jain <contact@atishay.me>
2018-08-13 18:50:55 -0700

    More cleanup

--- static/assets/video.js
+++
@@ -1,8 +0,0 @@
----
-# Concatenate all minified JavaScript files for video playback
----
-{% if jekyll.environment == 'production' %}
-{% include_relative _partials/js/plyr/plyr.polyfilled.min.js %}
-{% else %}
-{% include_relative _partials/js/plyr/plyr.polyfilled.js %}
-{% endif %}
