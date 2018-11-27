04c0045c5e4920f048c02fbc0a8fc5d93a7dd67e
Atishay Jain <contact@atishay.me>
2018-11-27 08:57:33 -0800

    Hover effects on phones

--- themes/jain/assets/js/main.js
+++ themes/jain/assets/js/main.js
@@ -15,33 +15,53 @@
   ///////////////////////////////////////////
   // Intersection Observer for animations
   ///////////////////////////////////////////
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(x => {
-      if (x.isIntersecting && !x.target.classList.contains('visible')) {
-        x.target.classList.add('visible');
-      }
-      if (x.target.classList.contains('visible')) {
-        observer.unobserve(x.target);
-        console.log("stopping", x.target);
-      } else {
-        console.log("Got event for ", x.target);
-      }
+  // Scroll animations for iPad and bigger
+  if (window.innerWidth >= 768) {
+    console.log("Greater");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(x => {
+        if (x.isIntersecting && !x.target.classList.contains('visible')) {
+          x.target.classList.add('visible');
+        }
+        if (x.target.classList.contains('visible')) {
+          observer.unobserve(x.target);
+        }
+      });
     });
-  });
-  // const currentScroll = window.pageYOffset;
-  ['.left-image', '.meta.default .item', '.item-icon-left .item', '.meta.default', '.max-2', '.max-2 .item', '.filter', '.filter .item',
-  '.blog', '.blog .item', 'footer .items', '.contact', '.more', '.comments', '.comments form',
-  '.item-icon-left', '.full-width', '.full-width .item'].forEach(s => {
+    ['.left-image', '.meta.default .item', '.item-icon-left .item', '.meta.default', '.max-2', '.max-2 .item', '.filter', '.filter .item',
+      '.blog', '.blog .item', 'footer .items', '.contact', '.more', '.comments', '.comments form',
+      '.item-icon-left', '.full-width', '.full-width .item'].forEach(s => {
 
-    document.querySelectorAll(s).forEach(x => {
-      const rect = x.getBoundingClientRect();
-      // All elements above the scroll are visible by default.
-      //  We only animate and scroll down.
-      if (rect.y < 0) {
-        x.classList.add('visible');
-      } else {
-        observer.observe(x);
-      }
+        document.querySelectorAll(s).forEach(x => {
+          const rect = x.getBoundingClientRect();
+          // All elements above the scroll are visible by default.
+          //  We only animate and scroll down.
+          if (rect.y < 0) {
+            x.classList.add('visible');
+          } else {
+            observer.observe(x);
+          }
+        });
+      });
+  } else {
+    console.log("Lesser");
+    // For mobile the hover animations
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(x => {
+        if (x.isIntersecting && !x.target.classList.contains('hover')) {
+            x.target.classList.add('hover');
+        } else if (!x.isIntersecting && x.target.classList.contains('hover')) {
+          x.target.classList.remove('hover')
+        }
+        console.log(x);
+      });
+    }, {
+      rootMargin: "-20% 0px -70% 0px"
     });
-  });
+    ['p', 'li', '.meta .content', '.meta .item-cover', '.item-icon', '.main-icon', '.banner-holder', '.img-container', '.tex', '.i-tex', '.post aside svg'].forEach(s => {
+      document.querySelectorAll(s).forEach(x => {
+          observer.observe(x);
+      });
+    });
+  }
 })();
