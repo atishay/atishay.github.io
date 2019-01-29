9996a42e219fd889b0b094db3ce6cb4dae2a0860
Atishay Jain <contact@atishay.me>
2019-01-28 19:47:41 -0800

    Added service worker.

--- themes/jain/assets/js/sw.js
+++ themes/jain/assets/js/sw.js
@@ -1,4 +1,3 @@
-// https://gist.github.com/adactio/fbaa3a5952774553f5e7
 (function () {
 
   // Update 'version' if you need to refresh the cache
@@ -23,9 +22,7 @@
       ].map(x =>  x.indexOf('http') === 0 ? new URL(x).pathname : x)));
   }
 
-  self.addEventListener('install', function (event) {
-    event.waitUntil(updateStaticCache());
-  });
+  self.addEventListener('install', (event) => event.waitUntil(updateStaticCache()));
 
   self.addEventListener('activate', function (event) {
     event.waitUntil(
@@ -40,30 +37,19 @@
     );
   });
 
+  function offline() {
+    return caches.match('/offline').then(x => x.text()).then(y => new Response(y, { "status": 200, headers: { 'Content-Type': 'text/html' } }));
+  }
+
   self.addEventListener('fetch', function (event) {
     const request = event.request;
     // Always fetch non-GET requests from the network
     if (request.method !== 'GET') {
-      event.respondWith(
-        fetch(request)
-          .catch(() => caches.match('/offline'))
-      );
-      return;
+      return event.respondWith(fetch(request).catch(offline));
     }
 
     // For HTML requests, try the network first, fall back to the cache, finally the offline page
     if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-      // return event.respondWith(caches.match('/offline'));
-      // Fix for Chrome bug: https://code.google.com/p/chromium/issues/detail?id=573937
-      if (request.mode != 'navigate') {
-        request = new Request(request.url, {
-          method: 'GET',
-          headers: request.headers,
-          mode: request.mode,
-          credentials: request.credentials,
-          redirect: request.redirect
-        });
-      }
       event.respondWith(
         fetch(request)
           .then(function (response) {
@@ -78,7 +64,7 @@
           .catch(function () {
             return caches.match(request)
               .then(function (response) {
-                return response || caches.match('/offline');
+                return response || offline()
               })
           })
       );
@@ -93,7 +79,7 @@
             .catch(function () {
               // If the request is for an image, show an offline placeholder
               if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
-                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"><title id="offline-title">Offline</title><path fill="rgba(145,145,145,0.5)" d="M0 0h400v225H0z" /><text fill="rgba(0,0,0,0.33)" font-family="Helvetica Neue,Arial,sans-serif" font-size="27" text-anchor="middle" x="200" y="113" dominant-baseline="central">offline</text></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
+                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"><title id="offline-title">Offline</title><path fill="rgba(145,145,145,0.5)" d="M0 0h400v225H0z" /><text fill="rgba(0,0,0,0.33)" font-family="Helvetica Neue,Arial,sans-serif" font-size="27" text-anchor="middle" x="200" y="113" dominant-baseline="central">Offline</text></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
               }
               return new Response('');
             });
