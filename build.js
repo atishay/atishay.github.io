d8a3d98bcd1860d59ae2c2b6cf5d19ba9a133dff
Atishay Jain <contact@atishay.me>
2019-11-22 13:15:23 -0800

    More work.

--- build.js
+++ build.js
@@ -2,10 +2,12 @@ const cp = require("child_process");
 const rcs = require("rename-css-selectors");
 const path = require("path");
 (async () => {
-  cp.execSync("hugo --minify --baseURL $URL", { cwd: __dirname, stdio: [0, 1, 2] });
+  cp.execSync(`hugo --minify --baseURL ${process.env.DEPLOY_URL}`, { cwd: __dirname, stdio: [0, 1, 2] });
   rcs.loadMapping('./renaming_map.json');
-  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { newPath: 'public', cwd: path.resolve(__dirname, './public') }).catch(e => console.log(e, e.stack));
-      await rcs.generateMapping('./', { overwrite: true }).catch(e => console.log(e.stack));
-  })();
+  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { newPath: 'public', cwd: path.resolve(__dirname, './public') });
+  await rcs.generateMapping('./', { overwrite: true });
+  // cp.execSync("../node_modules/.bin/terser *.js --mangle-props keep_quoted -c -m", { cwd: path.resolve(__dirname, './public'), stdio: [0, 1, 2] });
+})();
+
 
 
