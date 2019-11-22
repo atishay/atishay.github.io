886090d3e557205278204a4fe4065345ef2884be
Atishay Jain <contact@atishay.me>
2019-11-22 13:01:30 -0800

    Add build.js

--- build.js
+++ build.js
@@ -2,9 +2,9 @@ const cp = require("child_process");
 const rcs = require("rename-css-selectors");
 const path = require("path");
 (async () => {
-  await cp.exec("hugo --minify --baseURL $URL", { cwd: __dirname, stdio: [0, 1, 2] });
-      rcs.loadMapping('./renaming_map.json');
-  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { overwrite: true, cwd: path.resolve(__dirname, './public') }).catch(e => console.log(e, e.stack));
+  cp.execSync("hugo --minify --baseURL $URL", { cwd: __dirname, stdio: [0, 1, 2] });
+  rcs.loadMapping('./renaming_map.json');
+  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { newPath: 'public', cwd: path.resolve(__dirname, './public') }).catch(e => console.log(e, e.stack));
       await rcs.generateMapping('./', { overwrite: true }).catch(e => console.log(e.stack));
   })();
 
