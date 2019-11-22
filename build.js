4cb3a36f34262f8191cd9962800c679cf1f7ec0e
Atishay Jain <contact@atishay.me>
2019-11-22 15:06:01 -0800

    Use terser js.

--- build.js
+++ build.js
@@ -1,12 +1,38 @@
 const cp = require("child_process");
 const rcs = require("rename-css-selectors");
 const path = require("path");
+const terser = require("terser");
+const glob = require("glob");
+const fs = require("fs");
+const posthtml = require("posthtml");
 (async () => {
   cp.execSync(`hugo --minify --baseURL ${process.env.DEPLOY_URL}`, { cwd: __dirname, stdio: [0, 1, 2] });
   rcs.loadMapping('./renaming_map.json');
   await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { newPath: 'public', cwd: path.resolve(__dirname, './public') });
   await rcs.generateMapping('./', { overwrite: true });
-  // cp.execSync("../node_modules/.bin/terser *.js --mangle-props keep_quoted -c -m", { cwd: path.resolve(__dirname, './public'), stdio: [0, 1, 2] });
+  glob("public/**/*.js", (err, matches) =>
+    matches.forEach(m =>
+      fs.writeFileSync(m, terser.minify(fs.readFileSync(m, { encoding: 'utf-8' }), {
+        compress: {
+          unsafe_arrows: true,
+          unsafe_comps: true,
+          unsafe_math: true,
+          unsafe_methods: true,
+          unsafe_proto: true,
+          unsafe_regexp: true,
+          unsafe_undefined: true,
+        },
+        warnings: true,
+        mangle: {
+          properties: true
+        }
+      }).code)
+    ));
+  // glob("public/index.html", (err, matches) =>
+  //   matches.forEach(m =>
+  //     posthtml([require('htmlnano')({}, require('htmlnano').presets.max)]).process(fs.readFileSync(m, 'utf-8')).then(r => fs.writeFileSync(m, r.html)).catch(e => console.log(e, e.stack))
+  //   )
+  // );
 })();
 
 
