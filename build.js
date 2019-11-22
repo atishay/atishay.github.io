bc2f5067602f350dddd1e227f489f83fc275961b
Atishay Jain <contact@atishay.me>
2019-11-22 12:56:14 -0800

    More work on config.

--- build.js
+++ build.js
@@ -1,9 +1,10 @@
 const cp = require("child_process");
 const rcs = require("rename-css-selectors");
+const path = require("path");
 (async () => {
   await cp.exec("hugo --minify --baseURL $URL", { cwd: __dirname, stdio: [0, 1, 2] });
       rcs.loadMapping('./renaming_map.json');
-  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { overwrite: true, cwd: 'public' }).catch(e => console.log(e, e.stack));
+  await rcs.process.auto(['**/*.js', '**/*.html', '**/*.css'], { overwrite: true, cwd: path.resolve(__dirname, './public') }).catch(e => console.log(e, e.stack));
       await rcs.generateMapping('./', { overwrite: true }).catch(e => console.log(e.stack));
   })();
 
